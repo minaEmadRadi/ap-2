c380a8479adec5b8c14553ec9e6ee93b6dcd7a75
minaEmadRadi <minaemad393@gmail.com>
2022-10-04 22:50:29 +0200

    baseApi: "https://fakestoreapi.com/",

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,6 @@ export class ProductsService {
 
   constructor(private _http: HttpClient) { }
   getAllProducts() {
-    return this._http.get('https://fakestoreapi.com/products');
+    return this._http.get(environment.baseApi + 'products');
   } // get products from data.json
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { SelectComponent } from './components/select/select.component';
 import { Router, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 
@@ -13,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
     SelectComponent
   ],
   imports: [
+    BrowserModule,
     HttpClientModule,
     CommonModule,
     RouterModule,

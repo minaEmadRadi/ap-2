38c81a4fa9c355063c9d0831c35d046114e9429d
minaEmadRadi <minaemad393@gmail.com>
2022-10-04 22:49:55 +0200

    gotoTop-alert-warning

--- src/app/products/components/all-products/all-products.component.ts
+++ src/app/products/components/all-products/all-products.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 
 @Component({
@@ -18,8 +18,40 @@ export class AllProductsComponent implements OnInit {
   getproducts() {
     this.service.getAllProducts().subscribe((res: any) => { this.products = res; console.log(res) },
       error => {
-        alert('error');
+        alert(error.message);
       })
   }
+
+
+  /** */
+  isShow: boolean | undefined;
+  topPosToStartShowing = 100;
+
+  @HostListener('window:scroll')
+  checkScroll() {
+
+    // window의 scroll top
+    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  // TODO: Cross browsing
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+  /** */
 
 }
